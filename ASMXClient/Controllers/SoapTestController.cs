5b83b8558e561656469ddaf009ed71bf0d2b156f
arvind singh <arv9135@outlook.com>
2021-10-29 06:46:29 +0530

    Add project files.

---
+++ ASMXClient/Controllers/SoapTestController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using pocASMX;
+using SoapCoreReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wcfServer;
+
+namespace ASMXClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SoapTestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<SoapTestController> _logger;
+
+        public SoapTestController(ILogger<SoapTestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("net-core-asmx-test")]
+        public async  Task<ActionResult<MyCustomModel>> Get()
+        {
+            var model = new MyCustomModel()
+            {
+                Id = 2,
+                Email = "soapCoreTested@gmail.com",
+                Name = "Arvind",
+            };
+
+            var client = new SampleServiceClient(SampleServiceClient.EndpointConfiguration.BasicHttpBinding_ISampleService);
+            var ress = await client.TestCustomModelAsync(model);
+            return Ok(ress);
+        }
+
+
+        [HttpGet("net-asmx-test")]
+        public async Task<ActionResult<HelloWorldResponse>> GetAsmx()
+        {
+
+            var client = new SoupDemoSoapClient(pocASMX.SoupDemoSoapClient.EndpointConfiguration.SoupDemoSoap);
+            var ress = await client.HelloWorldAsync();
+            return Ok(ress.Body.HelloWorldResult);
+        }
+
+        [HttpGet("wcf-test")]
+        public async Task<ActionResult<HelloWorldResponse>> GetWCF()
+        {
+
+            var client = new Service1Client(wcfServer.Service1Client.EndpointConfiguration.BasicHttpBinding_IService1);
+            var ress = await client.GetDataAsync(100);
+            return Ok(ress);
+        }
+    }
+}
---
+++ pocAsmxAndWCF/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pocAsmxAndWCF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pocAsmxAndWCF")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b7f31e8c-a97e-4bd2-8c2d-14f88d29e341")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
